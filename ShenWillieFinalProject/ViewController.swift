b3c176979ae7bdfa5ed1e81c266b691b4cdea905
Willie Shen <williesh@usc.edu>
2018-11-24 10:01:35 -0800

    Initial Commit

---
+++ ShenWillieFinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShenWillieFinalProject
+//
+//  Created by Willie Shen on 11/24/18.
+//  Copyright © 2018 Willie Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
